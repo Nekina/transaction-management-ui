c27177723913a1f978ccab4e6fb4f8f9ce6e624b
Nekina <joshuaacusta@gmail.com>
2025-06-20 02:36:22 +0800

    Moved util functions and initial transaction data to separate file

--- app.py
+++ app.py
@@ -1,49 +1,9 @@
 # Import libraries
 from flask import Flask, request, url_for, redirect, render_template
+from utils import get_transaction, upd_transaction, del_transaction, calculate_total_amount, transactions
 
 # Instantiate Flask functionality
 app = Flask(__name__)
-
-# Sample data
-transactions = [
-    {'id': 1, 'date': '2023-06-01', 'amount': 100},
-    {'id': 2, 'date': '2023-06-02', 'amount': -200},
-    {'id': 3, 'date': '2023-06-03', 'amount': 300}
-]
-
-# Utility functions
-# Extract a single transaction
-def get_transaction(id):
-    for entry in transactions:
-        if entry["id"] == id:
-            return entry
-    return None
-
-# Update a single transaction
-def upd_transaction(id, new_entry):
-    for entry in transactions:
-        if entry["id"] == id:
-            entry.update(new_entry)
-            break
-
-# Delete a single transaction
-def del_transaction(id):
-    for i, entry in enumerate(transactions):
-        if entry["id"] == id:
-            del transactions[i]
-            break
-
-# Calculate total amount
-def calculate_total_amount(transactions):
-    total = 0
-    for entry in transactions:
-        total += entry["amount"]
-
-    # Handle data type
-    if total.is_integer():
-        total = int(total)
-
-    return total
 
 # API endpoints
 # Read operation
